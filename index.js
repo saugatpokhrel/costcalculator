4a20cf3db731579b41cd60a091e6c9da53cecc7b
saugatpokhrel <82570435+saugatpokhrel@users.noreply.github.com>
2021-11-07 16:53:02 +0545

    Added Different Services.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let fee = {
   netflix: 9.99,
   spotify: 9.99,
   prime: 14.99,
+  canva:12.99,
   microsoft365: 9.99,
   github: 4,
   discord: 9.99,
@@ -17,7 +18,21 @@ let fee = {
   crackle: 4.99,
   espn: 6.99,
   paramountplus: 10,
-  appletvplus:4.99,
+  appletvplus: 4.99,
+  googlecloud: 1.99,
+  dropbox: 9.99,
+  nordvpn:11.95,
+  expressvpn: 12.95,
+  surfshark:12.95,
+  protonvpn:4,
+  cyberghost:12.99,
+  ipvanish:10.99,
+  xboxcloud: 14.99,
+  geforce: 9.99,
+  shadow: 24.99,
+  vortex: 9.99,
+  psnow: 9.99,
+  boosteriod: 17.32,
 }
 //Initializing the variables
 let totalprice = 0
@@ -40,6 +55,21 @@ let cracklecheck = document.querySelector("input[name=crackle]");
 let espncheck = document.querySelector("input[name=espn]");
 let paramountpluscheck = document.querySelector("input[name=paramountplus]");
 let appletvpluscheck = document.querySelector("input[name=appletvplus]");
+let googlecloudcheck = document.querySelector("input[name=googlecloud]");
+let dropboxcheck = document.querySelector("input[name=dropbox]");
+let nordvpncheck = document.querySelector("input[name=nordvpn]");
+let expressvpncheck = document.querySelector("input[name=expressvpn]");
+let surfsharkcheck = document.querySelector("input[name=surfshark]");
+let protonvpncheck = document.querySelector("input[name=protonvpn]");
+let cyberghostcheck = document.querySelector("input[name=cyberghost]");
+let ipvanishcheck = document.querySelector("input[name=ipvanish]");
+let xboxcloudcheck = document.querySelector("input[name=xboxcloud]");
+let geforcecheck = document.querySelector("input[name=geforce]");
+let shadowcheck = document.querySelector("input[name=shadow]");
+let vortexcheck = document.querySelector("input[name=vortex]");
+let psnowcheck = document.querySelector("input[name=psnow]");
+let boosteroidcheck = document.querySelector("input[name=boosteroid]");
+let canvacheck=document.querySelector("input[name=canva]");
 let finalprice = document.getElementById("finalprice");
 let result = document.getElementById("result");
 
@@ -191,10 +221,119 @@ microsoft365checkbox.addEventListener('click', function () {
   if (this.checked) {
     totalprice = totalprice + fee.microsoft365
   } else {
-totalprice = totalprice - fee.microsoft365
+    totalprice = totalprice - fee.microsoft365
   }
 }
 );
+googlecloudcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.googlecloud
+  } else {
+    totalprice = totalprice - fee.googlecloud
+  }
+}
+);
+expressvpncheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.expressvpn
+  } else {
+    totalprice = totalprice - fee.expressvpn
+  }
+}
+);
+xboxcloudcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.xboxcloud
+  } else {
+    totalprice = totalprice - fee.xboxcloud
+  }
+});
+geforcecheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.geforce
+  } else {
+    totalprice = totalprice - fee.geforce
+  }
+});
+shadowcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.shadow
+  } else {
+    totalprice = totalprice - fee.shadow
+  }
+});
+vortexcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.vortex
+  } else {
+    totalprice = totalprice - fee.vortex
+  }
+});
+psnowcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.psnow
+  } else {
+    totalprice = totalprice - fee.psnow
+  }
+});
+boosteroidcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.boosteriod
+  } else {
+    totalprice = totalprice - fee.boosteriod
+  }
+});
+dropboxcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.dropbox
+  } else {
+    totalprice = totalprice - fee.dropbox
+  }
+});
+nordvpncheck.addEventListener('click', function () {  
+  if (this.checked) {
+    totalprice = totalprice + fee.nordvpn
+  } else {
+    totalprice = totalprice - fee.nordvpn
+  }
+});
+surfsharkcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.surfshark
+  } else {
+    totalprice = totalprice - fee.surfshark
+  }
+});
+protonvpncheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.protonvpn
+  } else {
+    totalprice = totalprice - fee.protonvpn
+  }
+});
+cyberghostcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.cyberghost
+  } else {
+    totalprice = totalprice - fee.cyberghost
+  }
+});
+ipvanishcheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.ipvanish
+  } else {
+    totalprice = totalprice - fee.ipvanish
+  }
+});
+canvacheck.addEventListener('click', function () {
+  if (this.checked) {
+    totalprice = totalprice + fee.canva
+  } else {
+    totalprice = totalprice - fee.canva
+  }
+});
+
+
 
 
 
@@ -204,7 +343,11 @@ totalprice = totalprice - fee.microsoft365
 
 finalprice.addEventListener('click', function () {
   let totalround = Math.round(totalprice * 100) / 100
-  result.innerHTML = `Your final price is $ ${totalround}`
+  if (totalround == 0) {
+    result.innerHTML = `Your final price is $ ${totalround}, Please Check A Item.`
+  } else {
+    result.innerHTML = `Your final price is $ ${totalround}`
+  }
 }
 );
 
